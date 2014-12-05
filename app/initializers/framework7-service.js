1f59df69b32eb48a732f449ce41067bee23faf81
Paul Spieker <p.spieker@duenos.de>
2014-12-05 10:24:31 +0100

    remove junk

--- app/initializers/framework7-service.js
+++ app/initializers/framework7-service.js
@@ -1,5 +1,4 @@
 export function initialize(container, application) {
-  application.inject('application', 'f7', 'service:framework7')
   application.inject('route', 'f7', 'service:framework7')
   application.inject('controller', 'f7', 'service:framework7')
   application.inject('component', 'f7', 'service:framework7')
