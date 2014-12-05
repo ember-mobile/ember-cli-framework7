df18071d2b1c21dfaf87b5b1d24c64811efc55cd
Paul Spieker <p.spieker@duenos.de>
2014-12-05 11:12:36 +0100

    bugfix: do not initialize Framework7 JS

--- app/services/framework7.js
+++ app/services/framework7.js
@@ -4,6 +4,11 @@ var f7 = new Framework7({
   init: false
 });
 
+f7.f7Init = f7.init;
+f7.init = function() {
+  return this._super();
+};
+
 var preloaderTimeout = null;
 
 export default Ember.Object.extend(f7, {
