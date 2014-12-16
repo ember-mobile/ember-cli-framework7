2a59aa2775d87747b941c9146ce4698d8fc0e857
Paul Spieker <p.spieker@duenos.de>
2014-12-16 21:03:09 +0100

    Bugfix: animate removing swipeout list items

--- app/components/f7-swipeout.js
+++ app/components/f7-swipeout.js
@@ -8,8 +8,21 @@ export default Ember.Component.extend({
     var _this = this;
     this.get('f7').initSwipeout(this.$());
   },
-  
+
   willDestroyElement: function() {
-    // TODO: Trigger animation
+    var _this = this;
+    var parent = this.$().parent();
+    var index = parent.children().index(this.$());
+    var clone = this.$().clone();
+    clone.find('script').remove();
+    Ember.run.scheduleOnce('afterRender', function() {
+      var elem = parent.children()[index];
+      if(elem) {
+        $(parent.children()[index]).before(clone);
+      } else {
+        parent.append(clone);
+      }
+      _this.get('f7').swipeoutDelete(clone);
+    });
   }
 });
