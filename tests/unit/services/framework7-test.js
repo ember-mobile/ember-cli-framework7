0ba6cf445a4dc0ecd266dd21467f941e7e23029e
Paul Spieker <p.spieker@duenos.de>
2014-12-26 10:05:40 +0100

    f7 service tests

--- tests/unit/services/framework7-test.js
+++ tests/unit/services/framework7-test.js
@@ -4,12 +4,37 @@ import {
 } from 'ember-qunit';
 
 moduleFor('service:framework7', 'Framework7Service', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function() {
+// Here we ensure the existence of methods of the Framework7 service which is
+// used by this component.
+
+test('it has property sizeNavbars', function() {
   var service = this.subject();
-  ok(service);
+  equal(typeof service.sizeNavbars, 'function');
+});
+
+test('it has property initSearchbar', function() {
+  var service = this.subject();
+  equal(typeof service.initSearchbar, 'function');
+});
+
+test('it has property initPullToRefresh', function() {
+  var service = this.subject();
+  equal(typeof service.initPullToRefresh, 'function');
+});
+
+test('it has property pullToRefreshDone', function() {
+  var service = this.subject();
+  equal(typeof service.pullToRefreshDone, 'function');
+});
+
+test('it has property initSwipeout', function() {
+  var service = this.subject();
+  equal(typeof service.initSwipeout, 'function');
+});
+
+test('it has property swipeoutDelete', function() {
+  var service = this.subject();
+  equal(typeof service.swipeoutDelete, 'function');
 });
