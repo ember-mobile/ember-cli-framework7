9a0609ac8dbeec6afc6ca4090da45821b752c6f1
robeno <robenosk@gmail.com>
2016-01-27 11:42:42 +0100

    commented out app.import of css files. Css files should be imported in ember-cli-build.js; switched to minified version of framework7.js

--- index.js
+++ index.js
@@ -8,13 +8,21 @@ module.exports = {
     this.app = app;
     this._super.included(app);
 
+    //IMPORT CUSTOM EMBER-CLI-FRAMEWORK7 CSS
     app.import('vendor/css/ember-cli-framework7.css');
 
     var bowerDirectory = app.bowerDirectory;
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.css');
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.themes.css');
-    app.import(bowerDirectory + '/framework7/dist/js/framework7.js');
-    app.import(bowerDirectory + '/framework7/dist/js/framework7.js.map', {
+    
+    //MATERIAL DESIGN
+    //app.import(bowerDirectory + '/framework7/dist/css/framework7.material.css');
+    //app.import(bowerDirectory + '/framework7/dist/css/framework7.material.colors.css');
+
+    //IOS DESIGN
+    //app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.css');
+    //app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.colors.css');
+
+    app.import(bowerDirectory + '/framework7/dist/js/framework7.min.js');
+    app.import(bowerDirectory + '/framework7/dist/js/framework7.min.js.map', {
       destDir: 'assets'
     });
   }
